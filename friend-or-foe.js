2460f386dcdab94cb70f4d90b39746b75fa794c2
Rafael Crempe <rafaelcrempe@gmail.com>
2026-03-29 19:02:32 -0300

    new challenge completed

---
+++ friend-or-foe.js
@@ -0,0 +1,27 @@
+/* Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Input = ["Ryan", "Kieran", "Jason", "Yous"]
+Output = ["Ryan", "Yous"]
+
+Input = ["Peter", "Stephen", "Joe"]
+Output = []
+
+Input strings will only contain letters.
+Note: keep the original order of the names in the output. */
+
+function friend(friends){
+  
+    let output = []
+    
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i].length == 4 && isNaN(friends[i])) {
+            output.push(friends[i])
+        }
+    }
+
+    console.log(output);
+}
+
+friend(["Ryan", "Kieran", "Jason", "Yous", 1, "Rafa"]);
